204965cbcb44c217ca08636081b057d11ba9581a
Scott F <s@sfrieds3.net>
2025-05-06 10:34:02 -0500

    add go server

--- go/build_proto.sh
+++ go/build_proto.sh
@@ -2,4 +2,10 @@
 set -euox pipefail
 
 echo "Building protos..."
-protoc --go_out=./proto --go-grpc_out=./proto --proto_path ../proto message_server.proto
+protoc \
+  --go_out=./proto \
+  --go-grpc_out=./proto \
+  --go_opt=paths=source_relative \
+  --go-grpc_opt=paths=source_relative \
+  --proto_path=../proto \
+  message_server.proto
--- go/main.go
+++ go/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	pb "grpc-server/go/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+)
+
+type messageServer struct {
+	pb.UnimplementedMessageServerServer
+}
+
+func (s *messageServer) GetMessage(_ context.Context, message *pb.MessageRequest) (*pb.Message, error) {
+	log.Printf("Received message: %s", message)
+	return &pb.Message{Name: message.Name, Message: proto.String("Received")}, nil
+}
+
+func newServer() *messageServer {
+	s := &messageServer{}
+	return s
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	port := 50051
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterMessageServerServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
